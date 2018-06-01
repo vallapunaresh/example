941a30bd3fc6c46810dc08e87bd8e64adde8f314
naresh <vallapunaresh30@gmail.com>
2018-06-01 19:10:52 +0530

    This is example program

---
+++ ExampleProgram/Example/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Example
+{
+    class Program
+    {
+    static (int, int) GetTwoNumbers()
+    {
+        // Return 2 numbers in a ValueTuple.
+        return (10, 20);
+    }
+
+    static void Main()
+    {
+        var result = GetTwoNumbers();
+        Console.WriteLine(result.Item1);
+        Console.WriteLine(result.Item2);
+        }
+    }
+}
